c6e94245fcafba1e5e9eda97a649064506208598
lingaiahdanda <lingaiahdanda@gmail.com>
2023-07-12 15:21:30 -0500

    First Commit

---
+++ src/test/java/com/linga/springjenkins/SpringJenkinsApplicationTest.java
@@ -0,0 +1,16 @@
+package com.linga.springjenkins;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpringJenkinsApplicationTest {
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void main() {
+    }
+}
